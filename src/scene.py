8cfbb1455ea0b0174a4d6bd39c5888438db359f2
DrHouseOfComputers <112186613+DrHouseOfComputers@users.noreply.github.com>
2023-02-22 13:25:23 +0000

    rendering tileset

--- src/scene.py
+++ src/scene.py
@@ -8,9 +8,15 @@ class Scene:
         self._obstacles = pg.sprite.Group()
         self._player = Player((0, 0), [self._sprites], self._obstacles)
 
+        self.map = pg.transform.scale(pg.image.load(('assets/tilemaps/map.png').convert(), (1280*2, 720*2))
+        self.map_rect = self._map.get_rect()
+        
     def run(self):
+        self._screen.blit(self._map, self._map_rect)
         self._sprites.draw(self._screen) 
         self.update()
 
     def update(self):
         self._sprites.update()
+
+    
