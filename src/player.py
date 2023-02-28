8650b3e5fccbe43332113636f71d92d4efaba03a
Micheal O'Dwyer <32364206+michealodwyer26@users.noreply.github.com>
2023-02-28 23:10:25 +0000

    Completed walking animations.

--- src/player.py
+++ src/player.py
@@ -4,23 +4,22 @@ from src.globals import *
 class Player(pg.sprite.Sprite):
     def __init__(self, position, groups, collisions):
         super().__init__(groups)
-        self._sprite_sheet = pg.image.load("assets/characters/player.png").convert_alpha()
-        # # self.image = pg.image.load("assets/player.png").convert_alpha()
-        self.image = pg.transform.scale(self.load_frame((9, 17, 32, 32)), (64, 64))
-        self.rect = self.image.get_rect(topleft = position)
         self._collisions = collisions
         
-
         self._v = pg.math.Vector2()
         self._speed = 3
         self._in_centre = False
 
-        # self._animations = {'down': [], 'up': [], 'left': [], 'right': []}
+        self._animations = {'walk_down': [], 'walk_up': [], 'walk_left': [], 'walk_right': [],
+                            'attack_down': [], 'attack_up': [], 'attack_left': [], 'attack_right': []}
+        self._animation_state = 'walk_down'
         self._frame_index = 0
         self._animation_timer = 0
-        self.down_animation = []
+
         self.load_animations()
-        self.image = self.down_animation[self._frame_index]
+
+        self.image = self._animations[self._animation_state][self._frame_index]
+        self.rect = self.image.get_rect(topleft = position)
 
     def read_key_input(self):
         for e in pg.event.get():
@@ -30,12 +29,16 @@ class Player(pg.sprite.Sprite):
             if e.type == pg.KEYDOWN:
                 if e.key == pg.K_LEFT:
                     self._v.x = -1
+                    self._animation_state = "walk_left"
                 elif e.key == pg.K_RIGHT:
                     self._v.x = 1
+                    self._animation_state = "walk_right"
                 elif e.key == pg.K_DOWN:
                     self._v.y = 1
+                    self._animation_state = "walk_down"
                 elif e.key == pg.K_UP:
                     self._v.y = -1
+                    self._animation_state = "walk_up"
 
         pressed_keys = pg.key.get_pressed()
 
@@ -59,24 +62,30 @@ class Player(pg.sprite.Sprite):
     def update(self):
         self.read_key_input()
         self.move()
-        self.image = self.down_animation[self._frame_index]
-        if self._v.x > 0 or self._v.y > 0: 
-            self._animation_timer += 1
-        if self._animation_timer > 20:
+        self.animate() 
+
+    def animate(self):
+        self.image = self._animations[self._animation_state][self._frame_index]
+
+        self._animation_timer += 1
+
+        if self._animation_timer > 15:
             self._frame_index += 1
             if self._frame_index == 5:
                 self._frame_index = 0 
             self._animation_timer = 0
 
-    def load_frame(self, rect):
+    def load_frame(self, rect, sprite_sheet):
         rectangle = pg.Rect(rect)
         frame = pg.Surface(rectangle.size, pg.SRCALPHA)
-        frame.blit(self._sprite_sheet, (0, 0), rectangle)
+        frame.blit(sprite_sheet, (0, 0), rectangle)
         return pg.transform.scale(frame, (64, 64))
     
     def load_animations(self):
-        for x in range(9, 249, 48): 
-            self.down_animation.append(self.load_frame((x, 160, 32, 32)))
-        for x in range(9, 249, 48): 
-            self.down_animation.append(self.load_frame((x, 160, 32, 32)))
-        
+        for state in self._animations.keys():
+            if "walk" in state:
+                sprite_sheet = pg.image.load("assets/characters/player/{}.png".format(state)).convert_alpha()
+                for x in range(0, 193, 64):
+                    self._animations[state].append(pg.transform.scale(self.load_frame((x, 0, 64, 64), sprite_sheet), (128, 128)))
+                for x in range(0, 129, 64):
+                    self._animations[state].append(pg.transform.scale(self.load_frame((x, 64, 64, 64), sprite_sheet), (128, 128)))
