d7a4bfd36f2e383df9e295f344120a27afe51c52
Lyonstead <Tommylyons02@gmail.com>
2023-03-04 14:42:49 +0000

    Added Health text to Health Bar

--- src/ui.py
+++ src/ui.py
@@ -10,14 +10,20 @@ class UI:
         self.health_bar_rect = pg.Rect(10, 10, HEALTH_BAR_WIDTH, BAR_HEIGHT)
         self.exp_bar_rect = pg.Rect(10, 40, EXP_BAR_WIDTH, BAR_HEIGHT)
 
-    def load_bar(self, current, max_amount, bg_rect, colour):
+    def show_health_bar(self, current, max_amount, bg_rect, colour):
         pg.draw.rect(self.display_surface, '#222222', bg_rect)
 
         current_rect = bg_rect.copy()
         current_rect.width = bg_rect.width * (current / max_amount)
-        
+
         pg.draw.rect(self.display_surface, HEALTH_COLOUR, current_rect)
+
+        health_text = self.font.render(str(int(current)), False, '#EEEEEE')
+        x = current_rect[0] + 4
+        y = current_rect[1] + 5
+        health_bar_center = health_text.get_rect(topleft = (x,y))
+        self.display_surface.blit(health_text, health_bar_center)
 
     def display(self, player):
 
-        self.load_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOUR)
+        self.show_health_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOUR)
