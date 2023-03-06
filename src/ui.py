f9d22bce52b0955f77cbf9dadb1f9e9431cdb75c
Tommy <TommyLyons02@gmail.com>
2023-03-06 17:28:57 +0000

    Added Experience Bar

--- src/ui.py
+++ src/ui.py
@@ -24,6 +24,21 @@ class UI:
         health_bar_center = health_text.get_rect(topleft = (x,y))
         self.display_surface.blit(health_text, health_bar_center)
 
+    def show_exp_bar(self, current, max_amount, bg_rect, colour):
+        pg.draw.rect(self.display_surface, '#222222', bg_rect)
+
+        current_rect = bg_rect.copy()
+        current_rect.width = bg_rect.width * (current / max_amount)
+
+        pg.draw.rect(self.display_surface, EXP_CLOUR, current_rect)
+
+        exp_text = self.font.render(str(int(current)), False, '#EEEEEE')
+        x = current_rect[0] + 4
+        y = current_rect[1] + 5
+        exp_bar_center = exp_text.get_rect(topleft = (x,y))
+        self.display_surface.blit(exp_text, exp_bar_center)
+
     def display(self, player):
 
         self.show_health_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOUR)
+        self.show_exp_bar(player.exp, 1000, self.exp_bar_rect, HEALTH_COLOUR)
