63ec70d7dba7898a5090d7ad49a2970936314232
Tommy <TommyLyons02@gmail.com>
2023-03-09 16:16:15 +0000

    Added assets

---
+++ assets/tilemaps/TopDownHouse_FloorsAndWalls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="TopDownHouse_FloorsAndWalls" tilewidth="16" tileheight="16" tilecount="162" columns="18">
+ <image source="../../assets-20230222T112931Z-001/Team13/assets/tilesets/TopDownHouse_FloorsAndWalls.png" width="288" height="144"/>
+</tileset>
---
+++ assets/tilemaps/TopDownHouse_FurnitureState1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="TopDownHouse_FurnitureState1" tilewidth="16" tileheight="16" tilecount="234" columns="13">
+ <image source="../../assets-20230222T112931Z-001/Team13/assets/tilesets/TopDownHouse_FurnitureState1.png" width="208" height="288"/>
+</tileset>
